bf42b04c2b3ee8eb569639296491125c09ee4fd9
John Wright <johngeorge.wright@gmail.com>
2015-03-27 20:55:47 +0000

    The start of a CLI

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-require('babel/register');
+require('babel/register')({only: new RegExp('^' + __dirname)});
 module.exports = require('./lib/compiler');
 exports.plugins = require('./lib/compiler/plugins');
